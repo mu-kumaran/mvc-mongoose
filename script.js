762ed41547ba60b19588c169dc1c004dbf093c48
Manoj Kumar <mu.kumaran1991@gmail.com>
2025-03-24 12:50:06 +0530

    new

--- script.js
+++ script.js
@@ -2,13 +2,17 @@ console.log("mongoose is ready")
 const mongoose = require("mongoose")
 const User = require("./userModel") 
 //here User as a class of usermodel
+
+// Mongoose connection
 mongoose.connect("mongodb://127.0.0.1:27017/manojdb").then(()=>{
     console.log("db connected")
 }).catch(()=>{
     console.log("db error")
 })
 
-const usr = new User({rno:1002,sname:"vinoth kumar",mark:85})
+// Inserting data into mongodb through mongoose
+var hob = ["Books","Movies","Travel"]
+const usr = new User({rno:1006,sname:"THANGAVEL",mark:79,hobbies:hob,city:"covai"})
 usr.save().then(()=>{
     console.log("successfully inserted")
 })
--- userModel.js
+++ userModel.js
@@ -1,8 +1,40 @@
 const mongoose = require("mongoose")
 const userSchema = new mongoose.Schema({
-    rno: Number,
-    sname: String,
-    mark: Number
+    /*  Normal format
+       rno: Number,
+       sname: String,
+       mark: Number
+     */
+    rno: {
+        type:Number,
+        required:true
+    },
+    sname: {
+        type:String,
+        required:true,
+        // uppercase:true
+        lowercase:true
+    },
+    mark: {
+        min:0,
+        max:100,
+        type:Number,
+        required:true
+    },
+    createdAt:{
+        type: Date,
+        default: new Date()
+        // default: ()=>Date.now()
+    }, 
+    hobbies:[String],
+    city:{
+        type:String,
+        required:true,
+        validate:{
+            validator: c=> (c==="karur"||c==="chennai"||c==="covai"),
+            message: `city is not allowed`
+        }
+    }
 })
 
 const usermdl = mongoose.model("mmm",userSchema)
